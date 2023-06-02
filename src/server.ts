66191d035febe625b27cb721a6a382bdc7ebba32
sadik <po480373@gmai.com>
2023-06-02 11:57:32 +0600

    test

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './config'
 
 async function bootStrap() {
   try {
-    // const xc = 12;
+
     await mongoose.connect(config.db_url as string)
     console.log(`db connected!`)
     app.listen(config.port, () => {
