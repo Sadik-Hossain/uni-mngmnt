a7f7e02d6538b9b1bac80af9560c481a94b28dbd
sadik <po480373@gmai.com>
2023-06-02 11:53:49 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 // global middleware
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // routes
 // app.use("/",myRoute)
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello from server");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello from server')
+})
 
 // catch all route error
 // app.use((req, res, next) => {
@@ -35,4 +35,4 @@ app.get("/", (req: Request, res: Response) => {
 //   });
 // });
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootStrap() {
   try {
-    await mongoose.connect(config.db_url as string);
-    console.log(`db connected!`);
+    // const xc = 12;
+             await mongoose.connect(config.db_url as string)
+    console.log(`db connected!`)
     app.listen(config.port, () => {
-      console.log("Server started on", config.port);
-    });
-  } catch (error: any) {
-    console.log(error.message);
+      console.log('Server started on', config.port)
+    })
+  } catch (e) {
+    console.log((e as Error).message)
   }
 }
-bootStrap();
+bootStrap()
